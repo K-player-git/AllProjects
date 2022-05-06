ea3067b66a23a846e3520313ae0dcb85a0ae0139
kk <kk>
2022-05-06 08:04:45 +0530

    changed App.css

--- todo/src/Components/Todo.js
+++ todo/src/Components/Todo.js
@@ -1,51 +1,54 @@
 import React, { Component } from 'react'
+import './Todo.css'
 
 export default class Todo extends Component {
-    constructor(){
+    constructor() {
         super();
-        this.state={
-            tasks:[{task:"task1",id:1},{task:"task2",id:2},{task:"task3",id:3}],
-            currTask:""
+        this.state = {
+            tasks: [{ task: "task1", id: 1 }, { task: "task2", id: 2 }, { task: "task3", id: 3 }],
+            currTask: ""
         }
     }
 
-    handleTask = (e)=>{
+    handleTask = (e) => {
         this.setState({
             currTask: e.target.value
         })
     }
 
-    handleSubmit=()=>{
+    handleSubmit = () => {
         this.setState({
-            tasks:[...this.state.tasks,{task:this.state.currTask,id:this.state.tasks.length+1}],
-            currTask:""
+            tasks: [...this.state.tasks, { task: this.state.currTask, id: this.state.tasks.length + 1 }],
+            currTask: ""
         })
     }
 
-    handleDelete = (id)=>{
-        let newArr = this.state.tasks.filter((t)=>{
+    handleDelete = (id) => {
+        let newArr = this.state.tasks.filter((t) => {
             return t.id !== id;
         })
-        this.setState ({
-            tasks:[...newArr]
+        this.setState({
+            tasks: [...newArr]
         })
     }
 
     render() {
         return (
-            <div>
-                <input type="text" value={this.state.currTask} onChange={this.handleTask}/>
-                <button onClick={this.handleSubmit} disabled={!this.state.currTask}>Submit</button>
+            <div className="container">
+                <div className="tasks">
+                    <input type="text" value={this.state.currTask} onChange={this.handleTask}/>
+                    <button onClick={this.handleSubmit} disabled={!this.state.currTask}>Submit</button>
+                </div>
                 {/* <p>{this.state.tasks[0].task}</p> */}
-                <ul>
-                {
-                    this.state.tasks.map((tasko)=>(
-                        <li key={tasko.id}>
-                            <p>{tasko.task}</p>
-                            <button onClick={()=>this.handleDelete(tasko.id)}>Delete</button>
-                        </li>
-                    ))
-                }
+                <ul className='task-list'>
+                    {
+                        this.state.tasks.map((tasko) => (
+                            <li key={tasko.id}>
+                                <span>{tasko.task}</span>
+                                <button onClick={() => this.handleDelete(tasko.id)}>Delete</button>
+                            </li>
+                        ))
+                    }
                 </ul>
             </div>
         )
